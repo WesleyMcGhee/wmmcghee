6a495dd136433772523025876370c3556481e121
Wesley McGhee <wesmcghee6372@gmail.com>
2024-01-30 19:56:59 -0500

    added projects

---
+++ src/components/Projects-Section.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="projects">
+    <h1 class="temp-title">Projects</h1> 
+    <h2 class="temp-section">Coming Soon...</h2>
+  </section>
+</template>
+
+<style>
+  .projects {
+    height: 40vh;
+    width: 40%;
+    margin: 0 auto;
+  }
+
+  .temp-section {
+    text-align: center;
+    margin-top: 10vh;
+  }
+</style>
--- src/pages/Main-Page.vue
+++ src/pages/Main-Page.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import IntroSection from "../components/Intro-Section.vue";
   import ExperinceSection from "../components/Experince-Section.vue"
+  import ProjectSection from "../components/Projects-Section.vue";
 </script>
 
 <template>
   <IntroSection />
   <ExperinceSection />
+  <ProjectSection />
 </template>
 
