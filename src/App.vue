09ab1e20e69c162a2cf5c614e84f4614884781db
Wesley McGhee <wesmcghee6372@gmail.com>
2024-02-01 18:11:40 -0500

    added underconstruction blog site

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
   import { ref, computed } from "vue";
   import MainPage from "./pages/Main-Page.vue";
-  import Blog from "./pages/Blog.vue";
+  import BlogPage from "./pages/Blog-Page.vue";
   import NotFound from "./pages/Not-Found.vue";
   import HeaderCom from "./components/Header-Com.vue";
   import FooterCon from "./components/Footer-con.vue";
 
   const routes: any = {
     "/": MainPage,
-    "/blog": Blog,
+    "/blog": BlogPage,
   }
 
   const currentPath = ref(window.location.hash)
---
+++ src/pages/Blog-Page.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="temp-header">Under Construction</h1>
+  <div class="temp-img-con">
+    <img class="temp-img" src="/under-construction.gif" alt="Under construction" />
+  </div>
+</template>
+
+<style>
+  .temp-header {
+    margin-top: 5vh;
+    text-align: center;
+  } 
+
+  .temp-img-con {
+    height: 500px;
+    width: 700px;
+    margin: 10vh auto;
+  }
+
+  .temp-img {
+    height: inherit;
+    width: inherit;
+  }
+</style>
