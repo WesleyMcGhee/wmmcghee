6e054096ba8aeae23acd3e52262008e56a1f0986
Wesley McGhee <wesmcghee6372@gmail.com>
2024-02-08 20:10:04 -0500

    added admin auth page

---
+++ src/pages/Admin-Auth.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+  // This probably needs to be implemented differently
+  // But IDK how to implement AUTH in Vue, so this is a learning experince
+
+  export default {
+    data() {
+      return {
+        username: "",
+        password: "",
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<template>
+  <h1>Admin Sign In</h1>
+  <form>
+    <input v-model="username" placeholder="Username" />
+    <input v-model="password" placeholer="Password" />
+  </form>
+</template>
